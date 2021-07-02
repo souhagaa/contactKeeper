578c02781077cc62509e440530161df4ac8516df
souhagaa <souha.gaaloul@hotmail.com>
2021-07-02 21:43:49 +0100

    MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+// Connect Database
+connectDB()
 
 app.get('/', (req, res) => res.send('hello world'))
 
@@ -11,4 +13,4 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-app.listen(PORT, ()=> console.log(`Server started on`))
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
