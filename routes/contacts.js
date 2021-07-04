43d7528656eeb7638384e674a0e2fbe74d3126f8
souhagaa <souha.gaaloul@hotmail.com>
2021-07-04 17:23:58 +0100

    Contact model & Get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User')
+const Contact = require('../models/Contact')
+
+const auth = require('../middleware/auth')
+
 const router = express.Router();
 
-// @route       GET api/contacts
-// @desc        Get all users contacts
-// @access      Private
-router.get('/', (req, res) => {
-    res.send('Get all ocntacts')
+/**
+ * @swagger
+ * /api/contacts:
+ *   get:
+ *     summary: Get all user's contacts
+ *     description: Protected route to get all user's contacts
+ *     responses:
+ *      200
+*/
+router.get('/', auth, async (req, res) => {
+    
+    try {
+        // We're using the auth middleware so we have access 
+        // to the user through req.user
+        const contacts = await Contact.find({ user: req.query.id}).sort({ date: -1})
+        res.json(contacts)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error while fetching contacts')
+    }
+
 })
 
 // @route       POST api/contacts
@@ -15,16 +39,28 @@ router.post('/', (req, res) => {
     res.send('Add conatct')
 })
 
-// @route       PUT api/contacts/:id
-// @desc        Update conatct
-// @access      Private
+/**
+ * @swagger
+ * /api/contacts/:id:
+ *   put:
+ *     summary: Update conatct
+ *     description: Protected route to update a conatct
+ *     responses:
+ *      200
+*/
 router.put('/:id', (req, res) => {
     res.send('Update conatct')
 })
 
-// @route       DELETE api/contacts/:id
-// @desc        Delete conatct
-// @access      Private
+/**
+ * @swagger
+ * /api/contacts/:id:
+ *   delete:
+ *     summary: Delete conatct
+ *     description: Protected route to delete conatct
+ *     responses:
+ *      200
+*/
 router.delete('/:id', (req, res) => {
     res.send('Delete conatct')
 })
