c19a4d52951bbe901dbb29e7aae8eb43d1326493
souhagaa <souha.gaaloul@hotmail.com>
2021-07-04 16:11:34 +0100

    password hashed and user gets saved to db, jwt gets sent back

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
+const router = express.Router();
 const User = require('../models/User')
-
-// @route       POST api/users
-// @desc        Register a user
-// @access      Public
 
 /**
  * @swagger
  * /api/users:
  *   get:
- *     summary: Register a user
+ *     summary: Register a user given name, email and password
  *     description: Registers a user so he can have an account to keep their contacts
  *     responses:
  *      200
@@ -25,14 +24,53 @@ router.post('/', [
         .isEmail(),
     check('password', 'Please enter a password with 6 or more characters')
         .isLength({ min: 6})
-    ], (req, res) => {
+    ], async (req, res) => {
 
     const errors = validationResult(req)
     if(!errors.isEmpty()) {
-        console.log("error api/users");
         return res.status(400).json( {errors: errors.array() })
     }
-    res.send('passed')
+
+    const { name, email, password } = req.body;
+    try {
+        // check if user already exists
+        let user = await User.findOne({ email })
+        if (user) {
+            res.status(400).json({ msg: 'User already exists'})
+        }
+        
+        user = new User({
+            name,
+            email,
+            password
+        })
+
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save();
+
+        // object we want to send in the token, we want to send back the user's id
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn : 360000
+        }, (err, token) => {
+
+            if(err) throw err;
+            res.json({ token })
+        })
+        
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('server error while user registering')
+    }
+    
 })
 
 module.exports = router;
