f008e2c2d63dde59e5c7ed4cd70fbb6db3b25300
souhagaa <souha.gaaloul@hotmail.com>
2021-07-04 17:50:10 +0100

    Add Contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,7 +22,7 @@ router.get('/', auth, async (req, res) => {
     try {
         // We're using the auth middleware so we have access 
         // to the user through req.user
-        const contacts = await Contact.find({ user: req.query.id}).sort({ date: -1})
+        const contacts = await Contact.find({ user: req.user.id}).sort({ date: -1})
         res.json(contacts)
 
     } catch (err) {
@@ -35,8 +35,34 @@ router.get('/', auth, async (req, res) => {
 // @route       POST api/contacts
 // @desc        Add new conatct
 // @access      Private
-router.post('/', (req, res) => {
-    res.send('Add conatct')
+router.post('/', [ auth, [
+    check('name', 'Name is required').not().isEmpty()
+] ],async(req, res) => {
+     const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json( {errors: errors.array() })
+        }
+    const { name, email, phone, type} = req.body;
+
+    try {
+
+        const newContact = new Contact({
+            name, 
+            email, 
+            phone, 
+            type,
+            user: req.user.id
+        })
+
+        const contact = await newContact.save()
+        res.status(201).json(contact)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error while creating new contact')
+    }
+
+
+
 })
 
 /**
