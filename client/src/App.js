4cd32d1ef5032492855ebd6c931baccfb5e6e635
souhagaa <souha.gaaloul@hotmail.com>
2021-07-21 21:40:52 +0100

    Auth context and initial state

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,26 @@ import Home from './components/pages/Home'
 import About from './components/pages/About'
 
 import ContactState from './context/contact/contactState'
+import AuthState from './context/auth/authState'
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar/>
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/about' component={About}/>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar/>
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/about' component={About}/>
 
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
---
+++ client/src/context/auth/authState.js
@@ -0,0 +1,75 @@
+import React, { useReducer } from 'react'
+
+import AuthContext from './authContext'
+import authReducer from './authReducer'
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL ,
+    USER_LOADED ,
+    AUTH_ERROR ,
+    LOGIN_SUCCESS ,
+    LOGIN_FAIL ,
+    LOGOUT ,
+    CLEAR_ERRORS} from '../types'
+
+
+const AuthState = props => {
+
+    const initialState = {
+        token: localStorage.getItem('token'),
+        user: null,
+        isAuthenticated: null,
+        loading: true,
+        error: null
+    }
+
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    // Load user check which user is logged in and get it 
+
+    const loadUser = user => {
+        //dispatch({ type: USER_LOADED, payload: user })
+    }
+    // Register user
+
+    const registerUser = user => {
+
+    }
+
+    // Login user
+
+    const loginUser = user => {
+
+    }
+
+    // Logout user
+
+    const logoutUser = user => {
+
+    }
+
+    // Clear errors
+
+    const clearErrors = () => {
+
+    }
+
+    return (<AuthState.Provider
+        value={{
+            token: state.token,
+            isAuthenticated: state.isAuthenticated,
+            user: state.user,
+            loading: state.loading,
+            error: state.error,
+            loadUser,
+            registerUser,
+            loginUser,
+            logoutUser,
+            clearErrors
+        }}>
+        { props.children }
+    </AuthState.Provider>)
+
+}
+
+export default ContactState
