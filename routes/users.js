cd005ec16bde53da940d52a4f8aa14bf8984b2d3
souhagaa <souha.gaaloul@hotmail.com>
2021-08-05 10:54:57 +0100

    app deployed to https://shrouded-temple-63076.herokuapp.com/

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+require('dotenv').config()
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
